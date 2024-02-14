fa039179264d84e39f96fd709b70c3506a9698f9
Skirmantux <149015954+Skirmantux@users.noreply.github.com>
2024-02-14 20:48:36 +0200

    Pabaigtos klaidų išmetimo funkcijos ir pridėta medianos pasirinkimo galimybė

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,11 +13,28 @@ struct studentas
     double egzaminorez;
     double namudarburezsuma;
     double vidurkis;
-    double galutinisbalas;
+    double galutinisbalasvidurkis;
+    double mediana;
+    double galutinisbalasmediana;
 };
 void NetinkamaIvestis()
 {
     cout << "Netinkama ávestis. Programa iðjungiama...";
+}
+void Rikiavimas(double mas[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (mas[j] > mas[j + 1])
+            {
+                double temp = mas[j];
+                mas[j] = mas[j + 1];
+                mas[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main()
@@ -26,19 +43,18 @@ int main()
     int studentukiekis;
     cout << "Kiek bus studentø?: " << endl;
     cin >> studentukiekis;
-    if (studentukiekis > 0)
+    if (studentukiekis > 0 && studentukiekis < INT_MAX)
     {
-        cout << "kiek bus namu darbø?: " << endl;
-        double namudarbai;
+        cout << "kiek bus namø darbø?: " << endl;
+        int namudarbai;
         cin >> namudarbai;
         studentas stud[N];
-        if (namudarbai > 0)
+        if (namudarbai > 0 && namudarbai < INT_MAX)
         {
             for (int i = 0; i < studentukiekis; i++)
             {
-                cout << "Iveskite studento vardà ir pavaradæ: " << endl;
-                cin >> stud[i].vardas;
-                cin >> stud[i].pavarde;
+                cout << "Iveskite studento vardà ir pavardæ: " << endl;
+                cin >> stud[i].vardas >> stud[i].pavarde;
                 stud[i].namudarburezsuma = 0;
                 for (int j = 0; j < namudarbai; j++)
                 {
@@ -52,26 +68,65 @@ int main()
                         else
                         {
                             cout << "Netinkama ávestis. Áveskite skaièiø tarp 0 ir 10." << endl;
+                            cin.clear();
+                            cin.ignore(numeric_limits<streamsize>::max(), '\n');
                         }
                     }
                     stud[i].namudarburezsuma += stud[i].namudarburez[j];
                 }
-                cout << "Iveskite egzamino rezultatà: " << endl;
-                cin >> stud[i].egzaminorez;
-                stud[i].vidurkis = stud[i].namudarburezsuma / namudarbai;
-                stud[i].galutinisbalas = stud[i].vidurkis * 0.4 + stud[i].egzaminorez * 0.6;
-            }
 
-            cout << "Pavarde" << setw(15) << "Vardas" << setw(30) << "Galutinis balas (vid.)" << setw(30) << endl;
-            cout << "-----------------------------------------------------------" << endl;
-            for (int i = 0; i < studentukiekis; i++)
-            {
-                cout << stud[i].pavarde << "         " << stud[i].vardas << "                " << stud[i].galutinisbalas << fixed << setprecision(3) << endl;
+                Rikiavimas(stud[i].namudarburez, namudarbai);
+                if (namudarbai % 2 == 0)
+                {
+                    stud[i].mediana = (stud[i].namudarburez[namudarbai / 2] + stud[i].namudarburez[namudarbai / 2 - 1]) / 2.0;
+                }
+                else
+                {
+                    stud[i].mediana = stud[i].namudarburez[namudarbai / 2];
+                }
+                while (true)
+                {
+                    cout << "Iveskite egzamino rezultatà (turi bûti tarp 0 ir 10): " << endl;
+
+                    if (cin >> stud[i].egzaminorez && stud[i].egzaminorez >= 0 && stud[i].egzaminorez <= 10)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cout << "Netinkama ávestis. Áveskite skaièiø tarp 0 ir 10." << endl;
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    }
+                }
+                stud[i].vidurkis = stud[i].namudarburezsuma / namudarbai;
+                stud[i].galutinisbalasvidurkis = stud[i].vidurkis * 0.4 + stud[i].egzaminorez * 0.6;
+                stud[i].galutinisbalasmediana = stud[i].mediana * 0.4 + stud[i].egzaminorez * 0.6;
             }
+            cout << "Kurá galutinio balo skaièiavimo bûdà renkatës? (1 - vidurkis; 2 - mediana)" << endl;
+            int skaicbudas;
+            if (cin >> skaicbudas && skaicbudas == 1)
+            {
+                cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(30) << "Galutinis balas (vid.)" << endl;
+                cout << "---------------------------------------------------------------------------------------------------" << endl;
+                for (int i = 0; i < studentukiekis; i++)
+                {
+                    cout << setw(15) << stud[i].pavarde << setw(15) << stud[i].vardas << setw(30) << fixed << setprecision(2) << stud[i].galutinisbalasvidurkis;
+                }
+            }
+            else if (skaicbudas == 2)
+            {
+                cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(30) << "Galutinis balas (med.)" << endl;
+                cout << "---------------------------------------------------------------------------------------------------" << endl;
+                for (int i = 0; i < studentukiekis; i++)
+                {
+                    cout << setw(15) << stud[i].pavarde << setw(15) << stud[i].vardas << setw(30) << fixed << setprecision(2) << stud[i].galutinisbalasmediana;
+                }
+            }
+            else
+                NetinkamaIvestis();
         }
         else NetinkamaIvestis();
     }
     else NetinkamaIvestis();
 }
-
-
