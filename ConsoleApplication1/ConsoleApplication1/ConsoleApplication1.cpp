d86cbba6734e2d84829ae5fede025ddc7d1c15ca
Skirmantux <149015954+Skirmantux@users.noreply.github.com>
2024-03-07 00:05:06 +0200

    Galutinai pabaigta v0.2.1 versija.
    Pakeistas readme.md failas, įdėta galimybė matuoti procesų (read/write, process) laiką, sutvarkytos kitos iš anksčiau turėtos funkcijos.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 #include <locale>
 #include <iomanip>
-#include <numeric>   
+#include <numeric>
 #include <algorithm>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <chrono> // Added chrono library
 
 using namespace std;
 
@@ -28,6 +29,10 @@ struct Studentas
 void NetinkamaIvestis()
 {
     cout << "Netinkama ávestis. Programa iðjungiama...";
+}
+void NeraFailo()
+{
+    cout << "Failas nerastas. Programa iðjungiama...";
 }
 
 void Rikiavimas(vector<double>& mas)
@@ -89,12 +94,16 @@ bool medianoslyginimas(const Studentas& a, const Studentas& b)
 
 void PrintStudents(const vector<Studentas>& studentai)
 {
+    auto start_print = chrono::high_resolution_clock::now();
     cout << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(30) << "Galutinis balas (vid.)" << setw(30) << "Galutinis balas (med.)" << endl;
     cout << "---------------------------------------------------------------------------------------------------" << endl;
     for (const auto& stud : studentai)
     {
         cout << setw(15) << stud.pavarde << setw(15) << stud.vardas << setw(30) << fixed << setprecision(2) << stud.galutinisbalasvidurkis << setw(30) << fixed << setprecision(2) << stud.galutinisbalasmediana << endl;
     }
+    auto end_print = chrono::high_resolution_clock::now();
+    chrono::duration<double> write_time = end_print - start_print;
+    cout << "Rasymo i ekrana trukme: " << write_time.count() << " sekundes" << endl;
 }
 
 int main()
@@ -103,6 +112,8 @@ int main()
     srand(time(NULL));
     vector<Studentas> studentai;
     int pasirinkimas;
+    chrono::duration<double> process_time;
+    chrono::duration<double> read_time;
     cout << "Pasirinkite bûdà ávesti balus:\n1. Ávesti rankiniu bûdu\n2. Sugeneruoti atsitiktinius balus\n3. Sugeneruoti balus, vardus ir pavardes\n4. Skaitymas ið failo.\n5. Baigti programà.\nPasirinkimas: ";
     while (true) {
         if (cin >> pasirinkimas && (pasirinkimas == 1 || pasirinkimas == 2 || pasirinkimas == 3 || pasirinkimas == 4)) {
@@ -121,49 +132,81 @@ int main()
     while (true)
     {
         Studentas stud;
-        if (pasirinkimas == 1 || pasirinkimas == 2)
-        {
-            cout << "Áveskite studento vardà ir pavardæ. Norëdami baigti ávedimà, áveskite -1: ";
-            cin >> stud.vardas;
-            if (stud.vardas == "-1")
-                break;
-            cin >> stud.pavarde;
-        }
         if (pasirinkimas == 1) {
-            cout << "Áveskite namø darbø rezultatus. Norëdami baigti ávedimà, áveskite -1: ";
-            double namudarburez;
-            while (true)
-            {
-                if (!(cin >> namudarburez)) {
+            while (true) {
+                Studentas stud;
+                cout << "Áveskite studento vardà ir pavardæ. Norëdami baigti ávedimà, áveskite -1: ";
+                cin >> stud.vardas;
+                if (stud.vardas == "-1")
+                    break;
+                cin >> stud.pavarde;
+                if (stud.pavarde == "-1")
+                    break;
+                cout << "Áveskite namø darbø rezultatus. Norëdami baigti ávedimà, áveskite -1: ";
+                while (true) {
+                    double namudarburez;
+                    if (!(cin >> namudarburez)) {
+                        cout << "Netinkama ávestis. Áveskite skaièiø tarp 0 ir 10." << endl;
+                        cin.clear();
+                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                        continue;
+                    }
+                    if (namudarburez == -1)
+                        break;
+                    if (namudarburez >= 0 && namudarburez <= 10) {
+                        stud.namudarburez.push_back(namudarburez);
+                    }
+                    else {
+                        cout << "Netinkama ávestis. Áveskite skaièiø tarp 0 ir 10." << endl;
+                    }
+                }
+                double egzaminorez;
+                cout << "Áveskite egzamino rezultatà: ";
+                cin >> egzaminorez;
+                if (egzaminorez >= 0 && egzaminorez <= 10) {
+                    stud.egzaminorez = egzaminorez;
+                }
+                else {
                     cout << "Netinkama ávestis. Áveskite skaièiø tarp 0 ir 10." << endl;
-                    cin.clear();
-                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
                     continue;
                 }
-                if (namudarburez == -1)
-                    break;
-                if (namudarburez >= 0 && namudarburez <= 10)
-                {
-                    stud.namudarburez.push_back(namudarburez);
-                }
-                else
-                {
-                    cout << "Netinkama ávestis. Áveskite skaièiø tarp 0 ir 10." << endl;
-                }
+
+                stud.namudarburezsuma = accumulate(stud.namudarburez.begin(), stud.namudarburez.end(), 0.0);
+                stud.vidurkis = stud.namudarburezsuma / stud.namudarburez.size();
+                stud.galutinisbalasvidurkis = stud.vidurkis * 0.4 + stud.egzaminorez * 0.6;
+                stud.mediana = Mediana(stud.namudarburez);
+                stud.galutinisbalasmediana = stud.mediana * 0.4 + stud.egzaminorez * 0.6;
+                studentai.push_back(stud);
             }
         }
         else if (pasirinkimas == 2) {
-            cout << "Generuojami atsitiktiniai balai uþ namø darbus..." << endl;
-            for (int i = 0; i < 5; ++i)
-            {
-                stud.namudarburez.push_back(GenerateRandomGrade());
+            while (true) {
+                cout << "Áveskite studento vardà ir pavardæ. Norëdami baigti ávedimà, áveskite -1: ";
+                cin >> stud.vardas;
+                if (stud.vardas == "-1")
+                    break;
+                cin >> stud.pavarde;
+                cout << "Generuojami atsitiktiniai balai uþ namø darbus..." << endl;
+                for (int i = 0; i < 5; ++i) {
+                    stud.namudarburez.push_back(GenerateRandomGrade());
+                }
+                stud.egzaminorez = GenerateRandomGrade();
+                stud.namudarburezsuma = accumulate(stud.namudarburez.begin(), stud.namudarburez.end(), 0.0);
+                stud.vidurkis = stud.namudarburezsuma / stud.namudarburez.size();
+                stud.galutinisbalasvidurkis = stud.vidurkis * 0.4 + stud.egzaminorez * 0.6;
+                stud.mediana = Mediana(stud.namudarburez);
+                stud.galutinisbalasmediana = stud.mediana * 0.4 + stud.egzaminorez * 0.6;
+                studentai.push_back(stud);
+                stud.namudarburez.clear();
             }
         }
         else if (pasirinkimas == 3) {
-            cout << "Generuojami balai, vardai ir pavardës spausdinimui" << endl;
-            int studentukiekis = rand() % 10;
-            for (int i = 0; i < studentukiekis; i++) {
-                for (int j = 0; j < 5; j++) {
+            cout << "Generuojami balai, vardai ir pavardës ir studentø kiekis spausdinimui" << endl;
+            int studentukiekis;
+            studentukiekis = rand() % 200;
+            for (int i = 0; i < studentukiekis; ++i) {
+                Studentas stud;
+                for (int j = 0; j < 5; ++j) {
                     stud.namudarburez.push_back(GenerateRandomGrade());
                 }
                 stud.egzaminorez = GenerateRandomGrade();
@@ -177,10 +220,10 @@ int main()
                 studentai.push_back(stud);
                 stud.namudarburez.clear();
             }
-            break;
         }
         else if (pasirinkimas == 4)
         {
+            auto start_read = chrono::high_resolution_clock::now();
             cout << "Kuri faila norite atidaryti:\n1 - studentai10000.txt\n2 - studentai100000.txt\n3 - studentai1000000.txt\nPasirinkimas: ";
             int failopasirinkimas;
             cin >> failopasirinkimas;
@@ -197,14 +240,17 @@ int main()
                 failas.open("studentai1000000.txt");
                 break;
             default:
-                cout << "Netinkama ávestis. Programa iðjungiama...";
+                NeraFailo();
                 return -1;
             }
             if (!failas.is_open())
             {
-                cout << "Failas nerastas. Programa iðjungiama...";
+                NeraFailo();
                 return -1;
             }
+            auto end_read = chrono::high_resolution_clock::now();
+            read_time = end_read - start_read;
+            auto start_process = chrono::high_resolution_clock::now();
             string line;
             getline(failas, line);
             istringstream iss(line);
@@ -235,6 +281,8 @@ int main()
                 studentai.push_back(stud);
                 stud.namudarburez.clear();
             }
+            auto end_process = chrono::high_resolution_clock::now();
+            process_time = end_process - start_process;
             failas.close();
         }
         if (!studentai.empty())
@@ -266,9 +314,11 @@ int main()
             if (skaicbudas == 1)
             {
                 PrintStudents(studentai);
+                break;
             }
             if (skaicbudas == 2)
             {
+                auto start_writing = chrono::high_resolution_clock::now();
                 ofstream failasr("kursioku_duomenys.txt");
                 failasr << setw(15) << "Pavarde" << setw(15) << "Vardas" << setw(30) << "Galutinis balas (vid.)" << setw(30) << "Galutinis balas (med.)" << endl;
                 failasr << "---------------------------------------------------------------------------------------------------" << endl;
@@ -278,6 +328,11 @@ int main()
                 }
                 cout << "Áraðymas sëkmingas. Rezultatai iðsaugoti faile kursioku_duomenys.txt" << endl;
                 failasr.close();
+                auto end_writing = chrono::high_resolution_clock::now();
+                chrono::duration<double> writeFile_time = end_writing - start_writing;
+                cout << "Skaitymas ið failo truko: " << read_time.count() << " sekundes" << endl;
+                cout << "Mokiniø vidurkio/medianos skaièiavimas uþtruko: " << process_time.count() << " sekundes" << endl;
+                cout << "Áraðymas á failà truko: " << writeFile_time.count() << " sekundes" << endl;
                 break;
             }
             else
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-v.0.1 tai antrasis alpha subrelizas, kuriame pagrindinė idėja - parodyti mokinių pasiekimų skaičiavimo programos principą. Bandant vesti raides ar nelegalius skaičius (pvz. 11 kaip pažymį) programa į tą reaguoją ir atsako, iš naujo paprašo parašyti. Implementuota atskira medianos funkcija, dinaminiai c masyvai ir vektoriai. Source failai - v.0.1 c masyvai ir v.0.1 vektoriai, juos turime atidaryti norint pažiūrėti kodą. Norint kompiliuoti kodą per Visual Studio, reikia atidaryti .sln failą /ConsoleApplication1 aplanke.
+v0.2.1 - tai dar labiau patobulinta mokinių galutinio pažymio skaičiuoklė. v0.2 naujienos:
+-Skaitymas/rašymas į failus;
+-Chronometras matuojantis, kiek laiko užima kiekviena operacija (read/write, process);
+-Optimizuotas kodas;
+-Galimybė užbaigti darbą per meniu;
+-Mokinių rikiavimas pagal balus, vardus, pavardes;
+-----------------------------------------------------------------------------------------------------------------------------
+Benchmark'ų vidurkiai naudojant iš ankščiau sugeneruotus failus (naudojamas vidurkio galutinio balo skaičiavimo metodas):
+1) studentai10000.txt:
+Skaitymas - 0.793849 sekundės;
+Rašymas - 0.0056045 sekundės;
+Skaičiavimas - 0.085432 sekundės;
+2) studentai100000.txt:
+Skaitymas - 0.823045 sekundės;
+Rašymas - 0.0209729 sekundės
+Skaičiavimas - 0.729594 sekundės;
+3) studentai1000000.txt:
+Skaitymas - 0.860858 sekundės;
+Rašymas - 0.163746 sekundės;
+Skaičiavimas - 3.04418 sekundės;
+Instrukcijos, kaip paleisti programą:
+
+Source failas - ConsoleApplication1, jį turime atidaryti norint pažiūrėti kodą. Norint kompiliuoti kodą per Visual Studio, reikia atidaryti .sln failą /ConsoleApplication1 aplanke ir paspausti "Local Windows Debugger" mygtuką viršuje. Tada tereikia sekti instrukcijas ekrane norint naudoti programą.
+
+DĖMĖSIO!!! - studentų (studentai10000.txt...) iš ankščiau sugeneruoti failai nėra įkeliami į repozitoriją, nes jų dydžiai yra per dideli. Prašome juos užsipildyti patiems arba atsisiųsti iš interneto.
+
+
+
