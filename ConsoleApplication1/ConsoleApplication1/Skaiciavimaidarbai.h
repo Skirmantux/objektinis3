875046fae153f082cd6617234306dde3ae89dbd5
Skirmantux <149015954+Skirmantux@users.noreply.github.com>
2024-03-07 22:31:22 +0200

    Perkeltos visos funkcijos i header failus
    Perkeltos visos funkcijos i header failus

---
+++ ConsoleApplication1/ConsoleApplication1/Skaiciavimaidarbai.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <chrono>
+void NetinkamaIvestis();
+void NeraFailo();
+void Rikiavimas(vector<double>& mas);
+double Mediana(const vector<double>& namudarburez);
+double GenerateRandomGrade();
+void PrintStudents(const vector<Studentas>& studentai);
